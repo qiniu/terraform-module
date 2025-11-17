c859446b59c8402713c3fa9082e4c51981084ee2
zzq <i@zhangzqs.cn>
2025-11-17 17:04:55 +0800

    Revert "✨ feat(mysql/replication)：重写image_data.tf、variables.tf和versions.tf文件，添加镜像数据、变量和版本配置 (#2)"
    This reverts commit af0b200b4e437ac4076c349c61a2d05b83f8b9bd.

--- mysql/replication/versions.tf
+++ mysql/replication/versions.tf
@@ -1,18 +1,1 @@
-terraform {
-  required_version = "> 0.12.0"
-
-  required_providers {
-    qiniu = {
-      source  = "hashicorp/qiniu"
-      version = "~> 1.0.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "qiniu" {}
-
-provider "random" {}
+../common/common_versions.tf
