2059ce2a2087d95fd1e90d6f34aad4e0a9564ebf
zzq <i@zhangzqs.cn>
2026-07-02 15:02:14 +0800

    Revert "feat(openclaw): 新增 cloud_init_only 开关支持纯 cloud-init 部署 (#50)"
    This reverts commit 531739d793e4aaf757f48159ddd88520e9df6a4b.

--- openclaw/openclaw_scripts/model.tf
+++ openclaw/openclaw_scripts/model.tf
@@ -85,12 +85,8 @@ resource "terraform_data" "write_back_minified_models_json" {
   }
 }
 
-locals {
-  model_config_script = templatefile("${path.module}/templates/model.sh.tmpl", {
+output "model_config_script" {
+  value = templatefile("${path.module}/templates/model.sh.tmpl", {
     provider_json = local.provider_json
   })
-}
-
-output "model_config_script" {
-  value = local.model_config_script
 }
